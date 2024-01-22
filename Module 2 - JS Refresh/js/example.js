8be20ed4d1609530f139a9528bca69649a8e51a5
tdotdlo <pixelbox.webdev@gmail.com>
2024-01-22 13:47:12 -0500

    Import Export Explained

--- Module 2 - JS Refresh/js/example.js
+++ Module 2 - JS Refresh/js/example.js
@@ -1,1 +1,9 @@
+// Without Export Default, you can export as many variables as you want.
 export const firstLine = 'Hello World!';
+
+// This will import the value and not a variable.
+// Can only have ONE export default per module file
+export default 'Revisiting JS';
+
+//Multiple exports to show js import/export grouping
+export const weather = "It's snowing";
--- Module 2 - JS Refresh/js/main.js
+++ Module 2 - JS Refresh/js/main.js
@@ -1,3 +1,14 @@
+// This will import the VARIABLE from another file. IT HAS TO BE THE SAME NAME
 import { firstLine } from './example.js';
 
 console.log(firstLine);
+// This will import the VALUE from the file - import name can be anything
+import value from './example.js';
+
+console.log(value);
+
+// This will group multiple exports (NOT EXPORT DEFAULTS) and import it as an object from the other JS file
+import * as objName from './example.js';
+
+console.log(objName.firstLine);
+console.log(objName.weather);
