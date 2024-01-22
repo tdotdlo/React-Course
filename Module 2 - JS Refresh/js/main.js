3743218c589e8c21d72663632c9c1eb50fa0c534
tdotdlo <pixelbox.webdev@gmail.com>
2024-01-22 14:23:44 -0500

    Examples of regular functions and arrow functions

--- Module 2 - JS Refresh/js/main.js
+++ Module 2 - JS Refresh/js/main.js
@@ -12,3 +12,56 @@ import * as objName from './example.js';
 
 console.log(objName.firstLine);
 console.log(objName.weather);
+
+// Creates the function definition
+function firstFn() {
+	console.log(1 + 1);
+}
+
+//Invokes the function
+firstFn(); //2
+firstFn(); //2
+
+// Functions with params
+function fnWithParams(firstNum, secondNum) {
+	console.log(firstNum, secondNum);
+}
+// Output
+fnWithParams(1, 2); //1 2
+fnWithParams(3, 4); //3, 4
+
+// Functions with default values
+function fnWithDefaultVal(firstNum, secondNum, thirdNum = 3) {
+	console.log(firstNum, secondNum, thirdNum);
+}
+
+//Arrow Fn - default Values
+// const fnWithDefaultVal = (firstNum, secondNum, thirdNum = 3) => console.log(firstNum, secondNum, thirdNum);
+
+// Output
+fnWithDefaultVal(1, 2); //1, 2, 3 - uses default value
+fnWithDefaultVal(1, 2, 4); //1, 2, 4 - overwrites default value
+
+// Functions with Return Value
+function fnWithReturnVal(firstNum, secondNum) {
+	return firstNum + secondNum;
+}
+
+// Output
+const sumOfnum = fnWithReturnVal(2, 4);
+console.log(sumOfnum);
+
+// Create a fn that accepts three values and returns the result of the operations
+function totalOfNum(one, two, three = 1) {
+	return (one / two) * three;
+}
+
+// Arrow Fn
+// const totalOfNum = (one, two, three) => (one/two) * 3;
+
+// Output
+let answer = totalOfNum(10, 5);
+console.log(answer); // 2
+
+answer = totalOfNum(10, 5, 2);
+console.log(answer); //4
