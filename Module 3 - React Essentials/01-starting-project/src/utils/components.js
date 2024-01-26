2c25cd8bad7cb776a51e88d8bb351af95c8c0e7c
Darren Lo <pixelbox.webdev@gmail.com>
2024-01-26 14:46:08 -0500

    Updated array in data.js file

--- Module 3 - React Essentials/01-starting-project/src/utils/components.js
+++ Module 3 - React Essentials/01-starting-project/src/utils/components.js
@@ -1,4 +1,7 @@
 import components from '../assets/components.png';
+import jsxImage from '../assets/jsx-ui.png';
+import propsImage from '../assets/config.png';
+import stateImage from '../assets/state-mgmt.png';
 
 export const coreConceptsInfo = [
 	{
@@ -7,8 +10,21 @@ export const coreConceptsInfo = [
 		image: components,
 	},
 	{
-		title: 'Components',
-		description: 'The core UI Building Block',
-		image: components,
+		title: 'JSX',
+		description:
+			'Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.',
+		image: jsxImage,
+	},
+	{
+		title: 'Props',
+		description:
+			'Make components configurable (and therefore reusable) by passing input data to them.',
+		image: propsImage,
+	},
+	{
+		title: 'State',
+		description:
+			'React-managed data which, when changed, causes the component to re-render & the UI to update.',
+		image: stateImage,
 	},
 ];
