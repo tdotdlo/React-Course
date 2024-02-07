968df0a87b3484ebb81db53ded91bba84ca5c075
Darren Lo <pixelbox.webdev@gmail.com>
2024-02-07 14:53:27 -0500

    Added player 1 pieces to board

--- Module 3 - React Essentials/tic-tac-toe/src/components/Gameboard/Gameboard.jsx
+++ Module 3 - React Essentials/tic-tac-toe/src/components/Gameboard/Gameboard.jsx
@@ -1,14 +1,42 @@
+import { useState } from 'react';
 import { emptyGameboard } from '../../data';
 
 export const Gameboard = () => {
+	// sets thee state of the gameboard to be the multi-dimensional array
+	const [playerMove, setPlayerMove] = useState(emptyGameboard);
+
+	// handles where the piece goes when a player clicks on a square
+	// parameters consist of rowIndex and colIndex bc we need to update that field
+	const handlePlayerMove = (playerMoveRow, playerMoveCol) => {
+		console.warn('clicked');
+		// prevPlayerMove (curr state) is called by React automatically. This will update
+		// the STATE immediately rather than having a time in the future
+		setPlayerMove((prevPlayerMove) => {
+			// This will update the origial array in memory when the state update by React happens
+			// prevPlayerMove[rowMoved][colMoved] = 'X';
+
+			prevPlayerMove.map((innerArray, i) => console.log(innerArray, i));
+			const updatedBoard = [...prevPlayerMove.map((innerArray) => [...innerArray])];
+			updatedBoard[playerMoveRow][playerMoveCol] = 'X';
+
+			return updatedBoard;
+		});
+	};
+
 	return (
 		<ol id='game-board'>
-			{emptyGameboard.map((row, i) => (
-				<li key={i}>
+			{playerMove.map((row, rowIndex) => (
+				<li key={rowIndex}>
 					<ol>
 						{row.map((playerPiece, colIndex) => (
 							<li key={colIndex}>
-								<button>{playerPiece}</button>
+								<button
+									onClick={() => {
+										handlePlayerMove(rowIndex, colIndex);
+									}}
+								>
+									{playerPiece}
+								</button>
 							</li>
 						))}
 					</ol>
